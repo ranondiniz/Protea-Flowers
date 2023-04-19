0284356c0be9819bd2209751d186159640f62256
Ranon Diniz <115851280+ranondiniz@users.noreply.github.com>
2023-04-19 20:10:42 -0300

    Add files via upload

---
+++ components/script.js
@@ -0,0 +1,60 @@
+function test2(){
+    var linktest = document.getElementById('link2');
+    linktest.onclick = function () {
+        window.location = "../perfil_comprador/perfil_comprador.html";
+    };
+}
+
+function test(){
+    var linktest = document.getElementById('link');
+    linktest.onclick = function () {
+        window.location = "../perfil_vendedor/perfil_vendedor.html";
+    };
+}
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+
+    //Ativa o modal
+
+    $(".entrar").click(function(){
+            
+        $(".cliente-modal").fadeIn();
+        $(".cliente").fadeIn();
+        
+        $('body').css("overflow", "hidden");
+        
+    });
+
+    $(".Central-vendedor").click(function(){
+        
+        $(".vendedor-modal").fadeIn();
+        $(".vendedor").fadeIn();
+       
+        $('body').css("overflow", "hidden");
+       
+    });
+
+     $("#finalizarCompra").click(function(){
+            
+        $(".aprovado").fadeIn();
+        // $('body').css("overflow", "hidden");
+    });
+
+    //Fecha o modal
+
+    $(".img-fechar").click(function(){
+        $(".fundo-modal").fadeOut();
+        $(".modal").fadeOut();
+        $('body').css("overflow", "visible");   
+       
+    });
+
+    //Abrir menu mobile
+
+    $(".menu-img").click(function(){
+        $(".box-menu-mobile").toggle();
+    });
+
+});
